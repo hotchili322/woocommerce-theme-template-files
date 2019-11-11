06f1bdad0d1858b5403792d2fd7c3617a77f5449
hotchili322 <houman.alvandi@gmail.com>
2019-11-11 21:20:51 +0330

    hook javascript to project

--- functions.php
+++ functions.php
@@ -6,3 +6,10 @@ function load_stylesheets()
 	wp_enqueue_style('stylesheet');
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+function load_javascript()
+{
+	wp_register_script('custom', get_template_directory_uri(). '/app.js','jquery', 1, 'true');
+	wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
