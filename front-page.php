691dd3f8575b7bfe45c9c4779ff161f54997f82f
hotchili322 <houman.alvandi@gmail.com>
2019-11-12 01:47:23 +0330

    add div for slider

--- front-page.php
+++ front-page.php
@@ -3,7 +3,9 @@
 <?php get_header(); ?>
 
 <div id="hero">
-
+     <div class="container d-flex align-content-center justify-content-center h-100">
+         <h1>Welcome to my shop</h1>
+     </div>
 </div>
 
 
